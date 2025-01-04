0b3c3bd09c7b991e721206432347a4b875f027f0
lushlife99 <wjdcks282828@gmail.com>
2025-01-04 22:55:24 +0900

    fix(cart) : CartItem에 커버이미지 URL 추가

--- src/main/java/com/example/msamemberapi/application/dto/response/CartDto.java
+++ src/main/java/com/example/msamemberapi/application/dto/response/CartDto.java
@@ -20,6 +20,7 @@ public class CartDto implements Serializable {
     public static class CartItem implements Serializable {
         private Long bookId;
         private String name;
+        private String coverUrl;
         private int regularPrice;
         private int salePrice;
         private int quantity;
@@ -27,6 +28,7 @@ public class CartDto implements Serializable {
         public CartItem(BookDetailResponseDto bookDetail, long bookId, int quantity) {
             this.bookId = bookId;
             this.name = bookDetail.getTitle();
+            this.coverUrl = bookDetail.getCoverUrl();
             this.regularPrice = bookDetail.getRegularPrice();
             this.salePrice = bookDetail.getSalePrice();
             this.quantity = quantity;
